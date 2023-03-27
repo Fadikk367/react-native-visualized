75db3e8d17ed10d8e9090cb60d1c83916934df06
Adrian Furman <adrian.furman@deployed.pl>
2023-03-27 21:53:54 +0200

    Extract eslint configuration to dedicated file

--- example/metro.config.js
+++ example/metro.config.js
@@ -25,9 +25,8 @@ module.exports = {
 
     blacklistRE: exclusionList(
       modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      )
+        m => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
+      ),
     ),
 
     extraNodeModules: modules.reduce((acc, name) => {
