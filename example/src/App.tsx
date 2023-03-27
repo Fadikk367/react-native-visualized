6017ba971a792ca83b7dae228a51bce83baed5c2
Adrian Furman <adrian.furman@deployed.pl>
2023-03-27 21:42:50 +0200

    Adjust prettier config

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-import { StyleSheet, View, Text } from 'react-native';
 import { multiply } from 'react-native-visualized';
 
 export default function App() {
