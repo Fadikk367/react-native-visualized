52f6954386a6a8c725aa32409ce48160b4e5b30f
Adrian Furman <adrian.furman@deployed.pl>
2023-04-26 00:01:57 +0200

    Add controls for ProgressRing parameters in example component

---
+++ example/src/screens/components/ProgressRing/data.tsx
@@ -0,0 +1,18 @@
+export const ringsA = [
+  { label: 'Calories', color: '#d13b3b', value: 78, full: 100 },
+  { label: 'Steps', color: '#31cd23', value: 54, full: 100 },
+  { label: 'Time', color: '#5165f8', value: 15, full: 100 },
+];
+
+export const ringsB = [
+  { label: 'Calories', color: '#d13b3b', value: 56, full: 100 },
+  { label: 'Steps', color: '#31cd23', value: 78, full: 100 },
+  { label: 'Time', color: '#5165f8', value: 65, full: 100 },
+];
+
+export const ringsC = [
+  { label: 'Protein', color: '#55ace2', value: 87, full: 100 },
+  { label: 'Carbs', color: '#e2d242', value: 92, full: 100 },
+  { label: 'Fats', color: '#c65050', value: 46, full: 100 },
+  { label: 'Fiber', color: '#6ac866', value: 54, full: 100 },
+];
