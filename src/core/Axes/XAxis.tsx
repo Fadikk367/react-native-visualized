e039c425c42bcf895c3d4b4b9379595fdea2487a
Adrian Furman <adrian.furman@deployed.pl>
2023-05-05 23:53:31 +0200

    Define variables for basic XAxis element styles

--- src/core/Axes/XAxis.tsx
+++ src/core/Axes/XAxis.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Group, Line, Text, useFont } from '@shopify/react-native-skia';
 
 import AxisArrow from '../AxisArrow';
+import { degreesToRadians } from '../utils';
 import type { AxisProps } from './types';
 
 const XAxis = ({
@@ -15,8 +16,23 @@ const XAxis = ({
   mapDomainToCanvas,
 }: AxisProps) => {
   const font = useFont(fontSource, fontSize);
-  const dashSize = 3;
-  const baseLineWidth = 1;
+
+  // axis line
+  const axisLineWidth = 2;
+  const axisLineColor = 'black';
+
+  // ticks labels
+  const labelRotation = 0;
+  const labelColor = 'black';
+  const formatLabel = (tick: number): string => {
+    return tick.toFixed(0);
+  };
+
+  // ticks
+  const showTicks = true;
+  const tickSize = 3;
+  const tickWidth = 1;
+  const tickColor = 'black';
 
   const tickElements = ticks.map(tick => {
     if (!font) return null;
@@ -27,17 +43,26 @@ const XAxis = ({
 
     return (
       <Group key={tick} transform={[{ translateX: xOffset }]}>
-        <Line
-          p1={{ x: 0, y: -(dashSize + baseLineWidth / 2) }}
-          p2={{ x: 0, y: dashSize + baseLineWidth / 2 }}
-          strokeWidth={1}
-        />
-        <Text
-          x={-labelTextWidth / 2}
-          y={height / 2 + font.getSize() / 2}
-          text={tick.toFixed(0)}
-          font={font!}
-        />
+        {showTicks ? (
+          <Line
+            p1={{ x: 0, y: -(tickSize + axisLineWidth / 2) }}
+            p2={{ x: 0, y: tickSize + axisLineWidth / 2 }}
+            strokeWidth={tickWidth}
+            color={tickColor}
+            strokeJoin="round"
+          />
+        ) : null}
+        <Group
+          origin={{ x: 0, y: height / 2 }}
+          transform={[{ rotate: degreesToRadians(labelRotation) }]}>
+          <Text
+            x={-labelTextWidth / 2}
+            y={height / 2 + font.getSize() / 2}
+            text={formatLabel(tick)}
+            color={labelColor}
+            font={font}
+          />
+        </Group>
       </Group>
     );
   });
@@ -47,7 +72,8 @@ const XAxis = ({
       <Line
         p1={{ x: 0, y: 0 }}
         p2={{ x: width, y: 0 }}
-        strokeWidth={baseLineWidth}
+        strokeWidth={axisLineWidth}
+        color={axisLineColor}
       />
       {tickElements}
       {arrows && (
